942f20190f61764470425c50b87d589b3d42f8f0
nvbn <nvbn.rm@gmail.com>
2015-01-21 22:13:13 +0300

    Add functions for creating indexes

--- src/subman_maintain/core.clj
+++ src/subman_maintain/core.clj
@@ -1,6 +1,7 @@
 (ns subman-maintain.core
   (:require [clojurewerkz.elastisch.rest.document :as esd]
             [clojurewerkz.elastisch.rest :as esr]
+            [clojurewerkz.elastisch.rest.index :as esi]
             [monger.collection :as mc]
             [monger.core :as mg]
             [environ.core :refer [env]]))
@@ -11,6 +12,31 @@
 (def raw-db (mg/get-db raw-db-conn (env :raw-db-name)))
 
 (def db-conn (esr/connect (env :db-host)))
+
+; Indexes:
+
+(defn create-index!
+  []
+  (esi/create (get-dep :db-connection)
+              (env :index-name)
+              :mappings {"subtitle"
+                         {:properties {:show {:type "string"}
+                                       :season {:type "string"
+                                                :index "not_analyzed"}
+                                       :episode {:type "string"
+                                                 :index "not_analyzed"}
+                                       :name {:type "string"}
+                                       :lang {:type "string"}
+                                       :version {:type "string"}
+                                       :url {:type "string"
+                                             :index "not_analyzed"}
+                                       :source {:type "integer"}}}}))
+
+(defn create-raw-index!
+  []
+  (mc/ensure-index raw-db "subtitle" (array-map :url 1) {:uniquer true}))
+
+; Migrations:
 
 (defn from-index-to-raw-db!
   []
